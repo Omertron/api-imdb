9db53297357e987e6058c035c86711e1a0b44043
stuart.boston <stuart.boston@PC-1017.ultra-as.net>
2012-09-18 11:51:17 +0100

    Removed unused import

--- src/main/java/com/moviejukebox/imdbapi/wrapper/WrapperChartStarmeter.java
+++ src/main/java/com/moviejukebox/imdbapi/wrapper/WrapperChartStarmeter.java
@@ -1,6 +1,5 @@
 package com.moviejukebox.imdbapi.wrapper;
 
-import com.moviejukebox.imdbapi.model.ImdbChartMoviemeter;
 import com.moviejukebox.imdbapi.model.ImdbChartStarmeter;
 import java.util.Collections;
 import java.util.List;
