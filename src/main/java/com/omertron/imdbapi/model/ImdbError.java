aaca5782aab7cfb9cc48c21b0c53630143e5f7fc
Stuart Boston <Stuart.Boston@Gmail.com>
2015-03-28 10:48:08 +0000

    Set source version to 1.7

--- src/main/java/com/omertron/imdbapi/model/ImdbError.java
+++ src/main/java/com/omertron/imdbapi/model/ImdbError.java
@@ -2,14 +2,12 @@ package com.omertron.imdbapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.Collections;
-import java.util.Map;
 
 @JsonIgnoreProperties({"@meta","db"})
 public class ImdbError extends AbstractJsonMapping {
 
     @JsonProperty("@type")
-    private String type = "";
+    private String type;
 
     public String getType() {
         return type;
